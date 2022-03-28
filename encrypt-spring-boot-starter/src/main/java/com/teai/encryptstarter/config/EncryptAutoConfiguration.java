a6938f77e86dc254ebc41c25e79df910e7709505
wang1317727943 <53896925+wang1317727943@users.noreply.github.com>
2022-03-28 09:31:35 +0800

    Delete encrypt-spring-boot-starter directory

--- encrypt-spring-boot-starter/src/main/java/com/teai/encryptstarter/config/EncryptAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.teai.encryptstarter.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.teai.encryptstarter")
-public class EncryptAutoConfiguration {
-}
